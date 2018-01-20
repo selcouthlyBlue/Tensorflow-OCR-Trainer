fc22579bcadcb80949089761ad556e1e8c47bd69
jerome <jvgonzalvo@up.edu.ph>
2018-01-20 20:34:11 +0800

    Added freeze_graph

--- main/tfutils.py
+++ main/tfutils.py
@@ -116,3 +116,34 @@ def images_to_sequence(inputs):
 def train(model, model_dir, input_fn, monitors=None):
     classifier = learn.Estimator(model_fn=model.model_fn, params=model.params, model_dir=model_dir)
     classifier.fit(input_fn=input_fn, monitors=monitors)
+
+def freeze_graph(model_dir, output_node_names):
+    if not tf.gfile.Exists(model_dir):
+        raise AssertionError(
+            "Export directory doesn't exists. Please specify an export "
+            "directory: %s" % model_dir)
+
+    if not output_node_names:
+        print("You need to supply the name of a node.")
+        return -1
+
+    checkpoint = tf.train.get_checkpoint_state(model_dir)
+    input_checkpoint = checkpoint.model_checkpoint_path
+
+    absolute_model_dir = "/".join(input_checkpoint.split('/')[:-1])
+    output_graph = absolute_model_dir + "/frozen_model.pb"
+
+    with tf.Session(graph=tf.Graph()) as sess:
+        saver = tf.train.import_meta_graph(input_checkpoint + '.meta', clear_devices=True)
+
+        saver.restore(sess, input_checkpoint)
+
+        output_graph_def = tf.graph_util.convert_variables_to_constants(
+            sess,
+            tf.get_default_graph().as_graph_def(),
+            output_node_names
+        )
+
+        with tf.gfile.GFile(output_graph, "wb") as f:
+            f.write(output_graph_def.SerializeToString())
+        print("%d ops in the final graph." % len(output_graph_def.node))

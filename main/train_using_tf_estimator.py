b651b16bcae3ed694e7e81055b260e5b5a80affb
jerome <jvgonzalvo@up.edu.ph>
2018-01-21 08:43:06 +0800

    Changed order of functions for processing and architecture creation.

--- main/train_using_tf_estimator.py
+++ main/train_using_tf_estimator.py
@@ -14,9 +14,22 @@ def main(_):
     image_paths, labels = dataset_utils.read_dataset_list('../test/dummy_labels_file.txt')
     data_dir = "../test/dummy_data/"
     images = dataset_utils.read_images(data_dir=data_dir, image_paths=image_paths, image_extension='png')
-    print('Done reading images')
     images = dataset_utils.resize(images, (1596, 48))
-    images = dataset_utils.transpose(images)
+    print('Done reading images')
+
+    checkpoint_dir = "checkpoint/"
+    architecture = "cnnmdlstm"
+
+    if architecture == "cnnmdlstm":
+        model = CNNMDLSTMCTCModelFn(input_shape=[1, 1596, 48, 1], starting_filter_size=16,
+                                    learning_rate=0.001, optimizer=Optimizers.MOMENTUM, num_classes=80)
+        checkpoint_dir += "cnnmdlstm"
+    else:
+        images = dataset_utils.transpose(images)
+        model = GridRNNModelFn(input_shape=[-1, 1596, 48], num_hidden_units=128, num_classes=80,
+                               learning_rate=0.001, optimizer=Optimizers.MOMENTUM)
+        checkpoint_dir += "gridlstm"
+
     labels = dataset_utils.encode(labels)
     labels = dataset_utils.pad(labels, blank_token_index=80)
     x_train, x_test, y_train, y_test = dataset_utils.split(features=images, test_size=0.5, labels=labels)
@@ -28,18 +41,6 @@ def main(_):
         input_fn=validation_input_fn,
         every_n_steps=5
     )
-
-    checkpoint_dir = "checkpoint/"
-    architecture = "cnnmdlstm"
-
-    if architecture == "cnnmdlstm":
-        model = CNNMDLSTMCTCModelFn(input_shape=[1, 1596, 48, 1], starting_filter_size=16,
-                                    learning_rate=0.001, optimizer=Optimizers.MOMENTUM, num_classes=80)
-        checkpoint_dir += "cnnmdlstm"
-    else:
-        model = GridRNNModelFn(input_shape=[-1, 1596, 48], num_hidden_units=128, num_classes=80,
-                               learning_rate=0.001, optimizer=Optimizers.MOMENTUM)
-        checkpoint_dir += "gridlstm"
 
     classifier = learn.Estimator(model_fn=model.model_fn, params=model.params, model_dir=checkpoint_dir)
     classifier.fit(input_fn=train_input_fn, monitors=[validation_monitor])

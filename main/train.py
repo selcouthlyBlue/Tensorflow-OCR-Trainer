c9e70b2578443230cf2baa18f952c46a479c3662
jerome <jvgonzalvo@up.edu.ph>
2018-01-22 09:39:38 +0800

    Separated main from train_using_tf_estimator.

---
+++ main/train.py
@@ -0,0 +1,18 @@
+from optimizer_enum import Optimizers
+from train_using_tf_estimator import train
+
+def main():
+    train(labels_file='../test/dummy_labels_file.txt',
+          data_dir='../test/dummy_data/',
+          desired_image_size=(1596, 48),
+          architecture="cnnmdlstm",
+          num_hidden_units=16,
+          optimizer=Optimizers.MOMENTUM,
+          learning_rate=0.001,
+          batch_size=1,
+          test_fraction=0.5,
+          validation_steps=5)
+
+
+if __name__ == '__main__':
+    main()
--- main/train_using_tf_estimator.py
+++ main/train_using_tf_estimator.py
@@ -6,40 +6,38 @@ from tensorflow.contrib import learn
 
 from GridRNNCTCModel import GridRNNCTCModel
 from CNNMDLSTMCTCModel import CNNMDLSTMCTCModel
-from optimizer_enum import Optimizers
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-def main(_):
-    image_paths, labels = dataset_utils.read_dataset_list('../test/dummy_labels_file.txt')
-    data_dir = "../test/dummy_data/"
+def train(labels_file, data_dir, desired_image_size, architecture, num_hidden_units, optimizer, learning_rate,
+          batch_size, test_fraction, validation_steps):
+    image_paths, labels = dataset_utils.read_dataset_list(labels_file)
     images = dataset_utils.read_images(data_dir=data_dir, image_paths=image_paths, image_extension='png')
-    images = dataset_utils.resize(images, (1596, 48))
+    images = dataset_utils.resize(images, desired_image_size)
     print('Done reading images')
 
     checkpoint_dir = "checkpoint/"
-    architecture = "cnnmdlstm"
 
     if architecture == "cnnmdlstm":
-        model = CNNMDLSTMCTCModel(input_shape=[1, 1596, 48, 1], starting_filter_size=16,
-                                  learning_rate=0.001, optimizer=Optimizers.MOMENTUM, num_classes=80)
+        model = CNNMDLSTMCTCModel(input_shape=[batch_size, desired_image_size[0], desired_image_size[1], 1], starting_filter_size=num_hidden_units,
+                                  learning_rate=learning_rate, optimizer=optimizer, num_classes=80)
         checkpoint_dir += "cnnmdlstm"
     else:
         images = dataset_utils.transpose(images)
-        model = GridRNNCTCModel(input_shape=[-1, 1596, 48], num_hidden_units=128, num_classes=80,
-                                learning_rate=0.001, optimizer=Optimizers.MOMENTUM)
+        model = GridRNNCTCModel(input_shape=[batch_size, desired_image_size[0], desired_image_size[1]], num_hidden_units=num_hidden_units, num_classes=80,
+                                learning_rate=learning_rate, optimizer=optimizer)
         checkpoint_dir += "gridlstm"
 
     labels = dataset_utils.encode(labels)
     labels = dataset_utils.pad(labels, blank_token_index=80)
-    x_train, x_test, y_train, y_test = dataset_utils.split(features=images, test_size=0.5, labels=labels)
+    x_train, x_test, y_train, y_test = dataset_utils.split(features=images, test_size=test_fraction, labels=labels)
 
     train_input_fn = create_input_fn(x_train, y_train)
     validation_input_fn = create_input_fn(x_test, y_test)
 
     validation_monitor = learn.monitors.ValidationMonitor(
         input_fn=validation_input_fn,
-        every_n_steps=5
+        every_n_steps=validation_steps
     )
 
     classifier = learn.Estimator(model_fn=model.model_fn, params=model.params, model_dir=checkpoint_dir)
@@ -59,4 +57,4 @@ def create_input_fn(x, y, num_epochs=1, shuffle=True, batch_size=1):
 
 
 if __name__ == '__main__':
-    tf.app.run(main=main)
+    tf.app.run(main=train)

c825cfc4b03a67e8309ee9f8e1edc923c161684d
jerome <jvgonzalvo@up.edu.ph>
2018-01-22 11:26:47 +0800

    Removed unused function.

--- main/optimizer_enum.py
+++ main/optimizer_enum.py
@@ -5,7 +5,3 @@ class Optimizers(Enum):
     RMSPROP = "rmsprop"
     ADADELTA = "adadelta"
     MOMENTUM = "momentum"
-
-    @staticmethod
-    def get_optimizer(optimizer_name):
-        return getattr(Optimizers, optimizer_name)

88d54006a75ef287d7ba7f1046103ac0a7bb5d4a
jerome <jvgonzalvo@up.edu.ph>
2018-01-29 19:10:47 +0800

    Interchanged num_features with time_steps.
    The required input of separable_lstm has a shape of (batch_size, height, width, depth).

--- test/multidimensional_rnn_learning_test.py
+++ test/multidimensional_rnn_learning_test.py
@@ -9,7 +9,7 @@ class MultidimensionalRNNTest(tf.test.TestCase):
         self.time_steps = 1596
         self.batch_size = 3
         self.num_channels = 1
-        self.input_layer = tf.placeholder(tf.float32, [self.batch_size, self.time_steps, self.num_features, self.num_channels])
+        self.input_layer = tf.placeholder(tf.float32, [self.batch_size, self.num_features, self.time_steps, self.num_channels])
         self.labels = tf.sparse_placeholder(tf.int32)
 
     def test_simple_mdrnn(self):

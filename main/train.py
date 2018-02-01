1385fe945d6d8a5c4b88668b3cef245e4a19acc7
jerome <jvgonzalvo@up.edu.ph>
2018-02-01 10:59:02 +0800

    Added more functions to dataset_utils.
    -Separated grayscaling (binarization) and type conversion from image reading.
    -Modified resize method to accept height and width instead of shape.

--- main/train.py
+++ main/train.py
@@ -6,13 +6,14 @@ def main():
     train(labels_file='../test/dummy_labels_file.txt',
           data_dir='../test/dummy_data/',
           labels_delimiter=' ',
-          desired_image_size=(1596, 48),
+          desired_image_height=48,
+          desired_image_width=1596,
           architecture=Architectures.CNNMDLSTM,
           num_hidden_units=16,
           optimizer=Optimizers.MOMENTUM,
           learning_rate=0.001,
           test_fraction=0.5,
-          num_epochs=160,
+          num_epochs=1,
           validation_steps=10,
           batch_size=1)
 

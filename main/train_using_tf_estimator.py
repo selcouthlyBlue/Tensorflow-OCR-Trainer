c9743eb64e393392ba4824ec9f59eaaa7246beb9
jerome <jvgonzalvo@up.edu.ph>
2018-02-01 12:32:21 +0800

    Made some changes to seq_length processing.
    -Instead of getting the sequence_lengths from each sample, a numpy array is filled with the desired sequence_length (width).
    
    -In CNNMDLSTMCTCModel, the seq_len is divided every time it passes through a max_pooling layer to cater to the sequence_length requirement of ctc_loss.

--- main/train_using_tf_estimator.py
+++ main/train_using_tf_estimator.py
@@ -34,7 +34,7 @@ def train(labels_file, data_dir, desired_image_height, desired_image_width, arch
 
     train_input_fn = input_fn(
         x_feed_dict={"x": np.array(x_train),
-                     "seq_lens": dataset_utils.get_seq_lens(x_train)},
+                     "seq_lens": dataset_utils.set_seq_lens(desired_image_width, len(x_train))},
         y=np.array(y_train, dtype=np.int32),
         num_epochs=num_epochs,
         batch_size=batch_size
@@ -42,7 +42,7 @@ def train(labels_file, data_dir, desired_image_height, desired_image_width, arch
 
     validation_input_fn = input_fn(
         x_feed_dict={"x": np.array(x_test),
-                     "seq_lens": dataset_utils.get_seq_lens(x_test)},
+                     "seq_lens": dataset_utils.set_seq_lens(desired_image_width, len(x_test))},
         y=np.array(y_test, dtype=np.int32),
         batch_size=batch_size,
         shuffle=False

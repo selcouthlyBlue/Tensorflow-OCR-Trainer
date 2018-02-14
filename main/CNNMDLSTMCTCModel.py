e1f858fd572bf339ba8456bdb1c564e374b3bf67
jerome <jvgonzalvo@up.edu.ph>
2018-02-14 15:01:58 +0800

    Added mode to dropout.

--- main/CNNMDLSTMCTCModel.py
+++ main/CNNMDLSTMCTCModel.py
@@ -16,11 +16,39 @@ class CNNMDLSTMCTCModel(Model):
     @staticmethod
     def model_fn(features, labels, mode, params):
         starting_filter_size = params["starting_filter_size"]
-
-        input_layer = network_utils.reshape(features["x"], params["input_shape"])
-        seq_lens = network_utils.reshape(features["seq_lens"], [-1])
         sparse_labels = network_utils.dense_to_sparse(labels, eos_token=80)
 
+        logits, seq_lens = CNNMDLSTMCTCModel._net_fn(features, mode, params, starting_filter_size)
+
+        decoded, log_probabilities = network_utils.ctc_beam_search_decoder(inputs=logits, sequence_length=seq_lens)
+
+        predicted_outputs = network_utils.sparse_to_dense(decoded, name="output")
+        predictions = {
+            "decoded": predicted_outputs,
+            "log_probabilities": log_probabilities,
+            "logits": logits
+        }
+        if network_utils.is_predict(mode):
+            return network_utils.create_model_fn(mode, predictions=predictions)
+
+        loss = network_utils.ctc_loss(labels=sparse_labels, inputs=logits, sequence_length=seq_lens)
+        label_error_rate = network_utils.label_error_rate(y_pred=decoded, y_true=sparse_labels)
+        network_utils.add_to_summary("label_error_rate", label_error_rate)
+
+        metrics = {"label_error_rate": network_utils.create_metric(label_error_rate)}
+
+        if network_utils.is_evaluation(mode):
+            return network_utils.create_model_fn(mode, predictions=predictions, loss=loss, eval_metric_ops=metrics)
+
+        assert network_utils.is_training(mode)
+
+        train_op = network_utils.create_train_op(loss, params["learning_rate"], params["optimizer"])
+        return network_utils.create_model_fn(mode, predictions=predictions, loss=loss, train_op=train_op)
+
+    @staticmethod
+    def _net_fn(features, mode, params, starting_filter_size):
+        input_layer = network_utils.reshape(features["x"], params["input_shape"])
+        seq_lens = network_utils.reshape(features["seq_lens"], [-1])
         net = network_utils.conv2d(input_layer, starting_filter_size, 3)
         net = network_utils.max_pool2d(net, 2)
         seq_lens = network_utils.div(seq_lens, 2)
@@ -51,33 +79,8 @@ class CNNMDLSTMCTCModel(Model):
         net = network_utils.mdlstm(net, starting_filter_size * 10, cell_type="GLSTM")
         net = network_utils.dropout(net, 0.25, mode)
         net = network_utils.collapse_to_rnn_dims(net)
-        net = network_utils.get_logits(inputs=net,
-                                       num_classes=params["num_classes"],
-                                       num_steps=net.shape[1],
-                                       num_hidden_units=net.shape[-1])
-
-        loss = None
-        train_op = None
-
-        if not network_utils.is_inference(mode):
-            loss = network_utils.ctc_loss(labels=sparse_labels, inputs=net, sequence_length=seq_lens)
-            network_utils.add_to_summary("loss", loss)
-
-        if network_utils.is_training(mode):
-            train_op = network_utils.create_train_op(loss, params["learning_rate"], params["optimizer"])
-
-        decoded, log_probabilities = network_utils.ctc_beam_search_decoder(inputs=net, sequence_length=seq_lens)
-        dense_decoded = network_utils.sparse_to_dense(decoded, name="output")
-        label_error_rate = network_utils.label_error_rate(y_pred=decoded, y_true=sparse_labels)
-        network_utils.add_to_summary("label_error_rate", label_error_rate)
-
-        predictions = {
-            "decoded": dense_decoded,
-            "probabilities": log_probabilities,
-            "label_error_rate": label_error_rate
-        }
-
-        return network_utils.create_model_fn(mode=mode,
-                                             predictions=predictions,
-                                             loss=loss,
-                                             train_op=train_op)
+        logits = network_utils.get_logits(inputs=net,
+                                          num_classes=params["num_classes"],
+                                          num_steps=net.shape[1],
+                                          num_hidden_units=net.shape[-1])
+        return logits, seq_lens

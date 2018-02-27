0bc9e3a00fe08ca61c1a7cdb972b7ea35dad4491
jerome <jvgonzalvo@up.edu.ph>
2018-02-27 11:43:28 +0800

    Modified model parameters for test training.

--- test/dummy_train.py
+++ test/dummy_train.py
@@ -1,10 +1,10 @@
 from train_ocr import train
 
 def main():
-    train(model_config_file='../models/CNN_BiRNN_model.json',
+    train(model_config_file='../models/test_model.json',
           labels_file='dummy_labels_file.txt',
           data_dir='dummy_data/',
-          desired_image_height=64,
+          desired_image_height=128,
           desired_image_width=3200,
           test_fraction=0.3,
           num_epochs=1,

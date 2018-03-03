c57e02bd1a3dc8ac6bbc572758a9077c9a2f1e39
jerome <jvgonzalvo@up.edu.ph>
2018-03-03 11:00:25 +0800

    Removed unused import.

--- main/backend/tf/losses.py
+++ main/backend/tf/losses.py
@@ -1,8 +1,5 @@
 import tensorflow as tf
 
-from backend.tf.util_ops import get_sequence_lengths, dense_to_sparse
-
-
 def ctc_loss(labels, inputs, sequence_length, preprocess_collapse_repeated_labels=False,
              ctc_merge_repeated=True, inputs_are_time_major=True):
     return tf.reduce_mean(tf.nn.ctc_loss(labels, inputs, sequence_length,

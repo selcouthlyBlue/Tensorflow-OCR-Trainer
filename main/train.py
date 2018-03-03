062e056c50166ef6bd8184937b9175029990fc6c
Jerome Gonzalvo <jvgonzalvo@up.edu.ph>
2018-03-03 13:49:50 +0800

    modified parameters for training

--- main/train.py
+++ main/train.py
@@ -1,16 +1,16 @@
 from train_ocr import train
 
 def main():
-    train(model_config_file='../models/three_layer_cnn_birnn_ctc_model.json',
+    train(model_config_file='../models/three_layer_cmdrnn_ctc_model.json',
           labels_file='/home/kapitan/Desktop/Jerome/words.txt',
           data_dir='/home/kapitan/Desktop/Jerome/words/',
-          desired_image_height=128,
-          desired_image_width=128,
+          desired_image_height=168,
+          desired_image_width=168,
           test_fraction=0.3,
           num_epochs=160,
-          save_checkpoint_epochs=5,
-          batch_size=320,
-          max_label_length=32)
+          save_checkpoint_epochs=1,
+          batch_size=240,
+          max_label_length=21)
 
 
 if __name__ == '__main__':

24065e87dee25b237c228b7091ef1143cbe36683
Jerome Gonzalvo <jvgonzalvo@up.edu.ph>
2018-03-06 15:40:40 +0800

    updated training parameters and model

--- main/train.py
+++ main/train.py
@@ -1,15 +1,15 @@
 from train_ocr import train
 
 def main():
-    train(model_config_file='../models/three_layer_cmdrnn_ctc_model.json',
+    train(model_config_file='../models/three_layer_cnn_birnn_ctc_model.json',
           labels_file='/home/kapitan/Desktop/Jerome/words.txt',
           data_dir='/home/kapitan/Desktop/Jerome/words/',
-          desired_image_height=168,
-          desired_image_width=168,
+          desired_image_height=176,
+          desired_image_width=176,
           test_fraction=0.3,
           num_epochs=160,
           save_checkpoint_epochs=1,
-          batch_size=240,
+          batch_size=440,
           max_label_length=21)
 
 

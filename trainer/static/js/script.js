7655ef2c9f25ae9b4b34d3ba7963488e7441e8ad
jerome <jvgonzalvo@up.edu.ph>
2018-03-14 13:01:04 +0800

    Modifications to the UI.
    -Layers can now be dynamically added in create_network_architecture.

--- trainer/static/js/script.js
+++ trainer/static/js/script.js
@@ -1,10 +1,45 @@
-$(".button-collapse").sideNav();
 $(document).ready(function(){
-    $('.parallax').parallax();
-});
-$(document).ready(function(){
-    $('.slider').slider();
-});
-$(document).ready(function() {
     $('select').material_select();
+    $(".button-collapse").sideNav();
+    var layer_types = {
+        "conv2d": "conv2d",
+        "maxpool2d": "maxpool2d",
+        "l2_normalize": "l2_normalize",
+        "batch_norm": "batch_norm",
+        "mdrnn": "mdrnn",
+        "birnn": "birnn",
+        "dropout": "dropout",
+        "collapse_to_rnn_dims": "collapse_to_rnn_dims"
+    };
+    $('#add-layer').click(function (e) {
+        e.preventDefault();
+        $('#layers').append(
+            $('<li>').append(
+                $('<div>').append(
+                    $('<select>').attr(
+                        {
+                            "name": "network[][layer_type]",
+                            "class": "layer"
+                        }
+                    )
+                    .prop('required', true)
+                    .append($('<option>').attr({"value": ""})
+                        .prop('disabled', true)
+                        .prop('selected', true)
+                        .text("Select Layer Type")
+                    )
+                ).attr({"class": "input-field col s3"})
+            ).append($('<div>')
+                .append($('<a>')).attr({})
+            )
+                .attr({
+                "class": "collection-item row"
+            })
+        );
+        $('select').material_select();
+        $.each(layer_types, function (key, value) {
+
+            }
+        );
+    });
 });
